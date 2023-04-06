990250253cdc48609f5f552d297614ff371a2695
Mahdi <amiralmasi021@gmail.com>
2023-04-06 23:47:57 +0430

    Add components and pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
-
+import Header from "./Component/Header/Header"
+import Footer from './Component/Footer/Footer';
+import { BrowserRouter  , Route,Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './Component/Projects/Projects';
+import Contact from './Component/Contact/Contact';
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className="App">
+     <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="/projects" element={<Projects />}/>
+              <Route path="/contact" element={<Contact />}/>
+            </Routes>
+          </main>
+          <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,37 +1,15 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+
 import "./Header.css"
-const Header = ({setToken}) =>{
-  const logoutHandler= () =>{
-    setToken("");
-    localStorage.clear();
-  }
-    return <header className="bg-nav">
-        <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit" className='log-out' onClick={logoutHandler}>Logout</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-    </header>
+import { Link } from "react-router-dom"
+const Header = () =>{
+    return <nav className="navbar">
+        <div className="navbar-links">
+           <ul>
+            <Link to="/"><li>Home</li></Link>
+            <Link to="/projects"><li>Projects</li></Link>
+            <Link to="/contacts"><li>Contacts</li></Link>
+           </ul>
+        </div>
+    </nav>
 }
 export default Header
