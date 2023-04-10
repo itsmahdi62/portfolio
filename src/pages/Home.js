67d2f26a6b42ce9c6427eb3ba2463df6f536e02d
Mahdi <amiralmasi021@gmail.com>
2023-04-10 20:13:54 +0330

    Remove useless styles and add some styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import image from "../../src/asset/1680812300773.jpg"
 import "../App.scss"
+import {BsEyeFill, BsCamera , BsLaptop, BsFillCameraVideoFill} from 'react-icons/bs'; 
 const Home = () => {
     return ( <div className="home">
         <div className="home-intro">
@@ -11,30 +12,33 @@ const Home = () => {
             <div className="home-intro-image">
                 <img src={image} alt="#" className="my-img"/>
             </div>
-            <div className="home-specialization">
+        </div>
+        <div className="home-specialization">
                 <h2>SPECIALIZATION IN</h2>
                 <div className="bottom-line"></div>
                 <p>Photography as well as creating digital masterpicese and UI/UX layout for website and mobile aplications</p>
             </div>
-        </div>
-
         <div className="home-specialization-grid">
             <div>
+            <BsCamera size={40} />
                 <h3>Photography</h3>
                 <p>{" "}I create high-quality images with my professional equipment and edit them with Adobe photoshop. Photography is my best skill.</p>
             </div>
 
             <div>
+            <BsFillCameraVideoFill size={40}/>
                 <h3>Video Editing</h3>
                 <p>{" "} lorem</p>
             </div>  
 
             <div>
-                <h3>Photography</h3>
-                <p>{" "}I create high-quality images with my professional equipment and edit them with Adobe photoshop. Photography is my best skill.</p>
+            <BsLaptop size={40}/>
+                <h3>UI / UX</h3>
+                <p>{" "}AS UX / UI is my passion I am creating user-frindly masterpiece layouts for website and mobile applications and my clients absolutely love them !</p>
             </div>
 
             <div>
+            <BsEyeFill />
                 <h3>Photography</h3>
                 <p>{" "}I create high-quality images with my professional equipment and edit them with Adobe photoshop. Photography is my best skill.</p>
             </div>
